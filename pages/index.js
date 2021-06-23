f7169a65bbf702c5de81e5fd61dfb53d188a7adb
Pratik Gohil <pr.tikgohil@gmail.com>
2021-06-23 18:53:19 +0530

    styling

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,37 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
-      <h1 className={styles.h1}>Hello</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <span className={styles.headingBackground}>
+          Very very carefully chosen tutors
+        </span>
+      </div>
+      <div className={styles.content}>
+        <span className={styles.contentHeading}>
+          Cambridge & IB Mathematics Subject Choice
+        </span>
+        <div className={styles.courses}>
+          <ul>
+            <li className={styles.courseName}>IGCSE - Mathematics</li>
+            <li>Mathematics Core/Extended 0580</li>
+            <li>International Mathematics 0607</li>
+            <li>Additional Mathematics 0606</li>
+          </ul>
+          <ul>
+            <li className={styles.courseName}>AS-A Level</li>
+            <li>Mathematics 9709</li>
+            <li>Mathematics Further 9231</li>
+          </ul>
+          <ul>
+            <li className={styles.courseName}>IBDP</li>
+            <li>Mathematics: AA SL</li>
+            <li>Mathematics: AA HL</li>
+            <li>Mathematics: AI SL</li>
+            <li>Mathematics: AI HL</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
